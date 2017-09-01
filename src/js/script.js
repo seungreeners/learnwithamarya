a6a9ec17ea6008562e83668e68c34ac35ca50aa6
Olaolu Olawuyi <whiskydee@gmail.com>
2017-09-01 22:33:27 +0100

    Some cleanup to the script

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 // let's get started.
-var links = document.querySelectorAll( 'a[href="#"]' );
+var links = document.querySelectorAll('a[href="#"]');
 
-for ( var i = 0; i < links.length; i++ ) {
-  var link = links[ i ];
-  link.addEventListener( 'click', function( event ) {
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+  link.addEventListener('click', function(event) {
     event.preventDefault();
-  }, false );
+  }, false);
 }
 
 // get the iframe stuff.
-var webButton = document.getElementById( 'web' );
-var iframe = document.getElementsByTagName( 'iframe' )[0];
-var iframeDoc = ( iframe.contentDocument ) ? iframe.contentDocument : iframe.contentWindow.document;
+var webButton = document.getElementById('web');
+var iframe = document.getElementsByTagName('iframe')[0];
+var iframeDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
 
-console.log( iframeDoc );
+console.log(iframeDoc);
